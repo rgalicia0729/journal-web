2cc9516e9d7a18399aa4f8b96b2434c855ba0205
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-02-06 11:20:52 -0600

    start project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {JournalApp} from './JournalApp.jsx';
+
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <JournalApp />
   </React.StrictMode>,
 )

8821ba90eb442de8233b976812a9fe5e117119c9
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-02-08 08:20:55 -0600

    end of design

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,26 @@
+import {JournalLayout} from '../layout/JournalLayout';
+import {NoteView, NothingSelectedView} from '../views/index.js';
+import {IconButton} from "@mui/material";
+import {AddOutlined} from "@mui/icons-material";
 
 export const JournalPage = () => {
     return (
-        <>
-            <h1>Journal Page</h1>
-        </>
+        <JournalLayout>
+            {/*<NothingSelectedView />*/}
+            <NoteView/>
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{fontSize: 30}}/>
+            </IconButton>
+        </JournalLayout>
     );
 }
